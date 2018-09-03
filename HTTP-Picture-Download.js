3cbd53ed74ca943f7b0c53bff33e9dfa9a4c7d02
Yang Yu <nicholas.yu24@gmail.com>
2018-09-03 20:31:57 +0000

    Download picture

---
+++ HTTP-Picture-Download.js
@@ -0,0 +1,30 @@
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Headers: ', response.headers['content-type']);
+         console.log('Downloading image...');
+       })
+       .on('end', function(){
+        console.log('Download complete.');
+       })
+       /*
+       .pipe(fs.createWriteStream('./future.jpg').on('finish', function(){
+        console.log('Download complete.');
+       }));               // Note 4
+       */
+       .pipe(fs.createWriteStream('./future.jpg'));
+
+
+
+// Notes:
+// 1. `request.get` is equivalent to `request()`
+// 2. `request.on('error', callback)` handles any error
+// 3. `request.on('response, callback)` handles the response
+// 4. What is happening here?
